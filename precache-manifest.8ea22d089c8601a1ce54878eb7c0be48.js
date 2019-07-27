0f18d08dd76298081ef3c526b09726beb52fd612
ejazsayyad <ejazsayyad4@gmail.com>
2019-07-27 21:13:50 +0530

    Updates

---
+++ precache-manifest.8ea22d089c8601a1ce54878eb7c0be48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aaccbd7716e809e09b3a7220d061577",
+    "url": "/YoutubeBasic/index.html"
+  },
+  {
+    "revision": "bef6c348fa2a71090dcd",
+    "url": "/YoutubeBasic/static/css/main.bdd68630.chunk.css"
+  },
+  {
+    "revision": "ce5239291f1d8c93992f",
+    "url": "/YoutubeBasic/static/js/2.2f0a20ed.chunk.js"
+  },
+  {
+    "revision": "bef6c348fa2a71090dcd",
+    "url": "/YoutubeBasic/static/js/main.f7de4d2d.chunk.js"
+  },
+  {
+    "revision": "7733f26ee3b0ac64de97",
+    "url": "/YoutubeBasic/static/js/runtime~main.faa82ee8.js"
+  }
+]);
